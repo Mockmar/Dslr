6d439a2cd8392d75f90fabe05482246eed24416c
Jean-baptiste GAUTIER <jgautier@bess-f2r1s5.clusters.42paris.fr>
2025-03-04 19:15:21 +0100

    repartition done mais c'est mieux de faire la repartion des caracteristique dans chaque maison

--- describe.py
+++ describe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 
 def import_csv(path):
     return pd.read_csv(path)
@@ -18,7 +19,42 @@ def calculate_age(df, today_year):
     df['Age'] = df['Birthday'].dt.year
     return df['Age'].apply(lambda x: today_year - x)
 
-def 
+def repartion_non_numerical_features(df, column):
+    tmp_df = df[column].value_counts()
+    tmp_df['total'] = tmp_df.sum()
+    count = tmp_df.values.astype(int)
+    house = tmp_df.index.tolist()
+    percent = (count/count[-1] * 100).round(2)
+
+    data = {
+        'Statistic': ['Count', 'Percentage'],
+    }
+
+    for i in range(len(house)):
+        data[house[i]] = [count[i], percent[i]]
+
+    df_repartition = pd.DataFrame(data)
+    return df_repartition
+
+
+
+def describe(df):
+    house_df = repartion_non_numerical_features(df, 'Hogwarts House')
+    print('House Repartition')
+    print(house_df.to_string(index=False))
+    hand_df = repartion_non_numerical_features(df, 'Best Hand')
+    print(hand_df.to_string(index=False))
+    print('\n')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Please provide a path to the dataset')
+        sys.exit(1)
+    path = sys.argv[1]
+    df = import_csv('datasets/dataset_train.csv')
+    df = preprocesing(df)
+    # df = extract_numerical_features(df)
+    describe(df)
 
 
 
